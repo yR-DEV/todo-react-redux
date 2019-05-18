7d7d7df3ee4535de77db1269a0632fb65ba3c710
yeah-right <claar.zack@gmail.com>
2019-05-18 14:25:25 -0600

    Have basic redux structure in place, going to try and connect it to react

---
+++ src/js/reducers/index.js
@@ -0,0 +1,16 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
+const initialState = {
+    articles: []
+};
+
+const rootReducer = (state = initialState, action) => {
+    if (action.type === "ADD_ARTICLE") {
+        return Object.assign({}, state, {
+            articles: state.articles.concat(action.payload)
+        });
+    }
+    return state;
+}
+
+export default rootReducer;
